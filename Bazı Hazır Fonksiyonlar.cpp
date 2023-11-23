ecca27d37648511d567a01cf59a96441d22c4e2b
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-11-23 14:08:33 +0300

    Add files via upload

---
+++ Bazı Hazır Fonksiyonlar.cpp
@@ -0,0 +1,22 @@
+// Hazır Fonksiyonlar
+
+#include <iostream>
+#include <cstring>
+#include <math.h>
+
+
+int main(int argc, char *argv[])
+{
+
+isupper(); // Girilen karakterin büyük harf olup olmadığını kontrol eder.
+islower(); // Girilen karakterin küçük harf olup olmadığını kontrol eder.
+strlen(); // Girilen kelimenini harf sayısını bulur. - cstring kütüphanesi gerekidir.
+tolower(); // Girilen yazyıyı küçük harflere çevirir.
+toupper(); // Girilen yazıyı büyük harflere çevirir.
+sin(); 
+cos(); tan(); // hesaplamaya yarar. - math.h kütüphanesi gereklidir.
+
+
+
+    std::cout << "Hello world!" << std::endl;
+}
